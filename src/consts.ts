a1d3b11f9684495cb294be930aac009a4e176a64
KJ Roelke <kj.roelke@gmail.com>
2023-03-24 15:49:26 -0500

    add site_description global back

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "The Wiki for CNO's Dev Team.";
+export const SITE_DESCRIPTION =
+	'A wiki for the developers of the Choctaw Nation of Oklahoma';
