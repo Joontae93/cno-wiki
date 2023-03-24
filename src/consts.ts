f3eb2ae40f13ef86691798ddad8135c9333f6810
KJ Roelke <kj.roelke@gmail.com>
2023-03-24 15:32:27 -0500

    update styles and fix bugs

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,3 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "The Wiki for CNO's Dev Team.";
-export const SITE_DESCRIPTION = 'Built with ❤️ and Astro.js';
